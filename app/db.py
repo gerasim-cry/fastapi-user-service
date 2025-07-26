f1072a7742a8e4872294895303ef6dcc8ec2225e
gerasim <gerasimvvv0@gmail.com>
2025-07-26 16:47:31 +0400

    New10 initial commit with CI

--- app/db.py
+++ app/db.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://user:password@db:5432/appdb"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg2://user:password@db:5432/appdb")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
